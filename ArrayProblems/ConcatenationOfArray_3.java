155973dd43b48207a97cda232f01bc149e66f6e9
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-03-05 11:29:53 +0530

    Update ConcatenationOfArray_3.java

--- ArrayProblems/ConcatenationOfArray_3.java
+++ ArrayProblems/ConcatenationOfArray_3.java
@@ -30,13 +30,18 @@ n == nums.length
 1 <= nums[i] <= 1000
 
  */
+
 package ArrayProblems;
+
 import java.util.Arrays;
+
 public class ConcatenationOfArray_3 {
+    
     public static void main(String[] args){
         int[] arr = {1,3,2,1};
         System.out.println(Arrays.toString(getConcatenation(arr)));
     }
+    
     public static int[] getConcatenation(int[] nums) {
         int[] ans = new int[2*nums.length];
         for(int i=0;i<nums.length;i++){
@@ -45,6 +50,7 @@ public class ConcatenationOfArray_3 {
         }
         return ans;
     }
+    
 }
 
 /*
