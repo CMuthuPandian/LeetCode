778b5fd0cade6ba6d1e59011dce6930a9837103c
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-03-07 22:32:25 +0530

    Update FindSingleElement_5.java

--- ArrayProblems/FindSingleElement_5.java
+++ ArrayProblems/FindSingleElement_5.java
@@ -47,11 +47,9 @@ Other poossible answers wth less time complexicity:
 
 public int singleNumber(int[] nums) {
     int ans =0;
-
     int len = nums.length;
     for(int i=0;i!=len;i++)
         ans ^= nums[i];
-
     return ans;
 }
 
