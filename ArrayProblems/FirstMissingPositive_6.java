73e9863ccbf2d555364bf901c4ca92c3635eac7f
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-03-09 00:23:29 +0530

    Update FirstMissingPositive_6.java

--- ArrayProblems/FirstMissingPositive_6.java
+++ ArrayProblems/FirstMissingPositive_6.java
@@ -1,7 +1,6 @@
 /*
 
 Given an unsorted integer array nums, return the smallest missing positive integer.
-
 You must implement an algorithm that runs in O(n) time and uses constant extra space.
 
 Example 1:
