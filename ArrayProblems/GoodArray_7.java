2c92bb3b8917ba83682b2ecc51cf7a1852e28f87
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-03-10 23:42:21 +0530

    Add files via upload

---
+++ ArrayProblems/GoodArray_7.java
@@ -0,0 +1,60 @@
+/*
+
+Given an array nums of positive integers. Your task is to select some subset of nums,
+multiply each element by an integer and add all these numbers.
+The array is said to be good if you can obtain a sum of 1 from the array by any possible subset and
+multiplicand.
+
+Return True if the array is good otherwise return False.
+
+Example 1:
+
+Input: nums = [12,5,7,23]
+Output: true
+Explanation: Pick numbers 5 and 7.
+5*3 + 7*(-2) = 1
+
+Example 2:
+
+Input: nums = [29,6,10]
+Output: true
+Explanation: Pick numbers 29, 6 and 10.
+29*1 + 6*(-3) + 10*(-1) = 1
+
+Example 3:
+
+Input: nums = [3,6]
+Output: false
+
+Constraints:
+
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^9
+
+ */
+
+package ArrayProblems;
+
+public class GoodArray_7 {
+
+    public static void main(String[] args){
+        int[] arr = {3,6};
+        System.out.println(isGoodArray(arr));
+    }
+
+    public static boolean isGoodArray(int[] nums) {
+        int result =  nums[0];
+        for(int i=0;i<nums.length;i++){
+            result = gcd(nums[i],result);
+        }
+        return result==1;
+    }
+
+    public static int gcd(int i, int j){
+        if(j==0)
+            return i;
+        else
+            return gcd(j,i%j);
+    }
+
+}
