c42273f08d053c1c887040df8fc7184e976afbee
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-03-23 21:16:15 +0530

    Update RunningSum1dArray_20.java

--- ArrayProblems/RunningSum1dArray_20.java
+++ ArrayProblems/RunningSum1dArray_20.java
@@ -1,7 +1,6 @@
 /*
 
-Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
-Return the running sum of nums.
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).Return the running sum of nums.
 
 Example 1:
 
