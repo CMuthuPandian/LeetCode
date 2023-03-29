7595274f07eda50feb4ea6e2aeb4b80b9c4b0cd7
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-03-29 23:11:02 +0530

    Update CountConsistentString_2.java

--- HashTable/CountConsistentString_2.java
+++ HashTable/CountConsistentString_2.java
@@ -2,7 +2,6 @@
 
 You are given a string allowed consisting of distinct characters and an array of strings words.
 A string is consistent if all characters in the string appear in the string allowed.
-
 Return the number of consistent strings in the array words.
 
 Example 1:
