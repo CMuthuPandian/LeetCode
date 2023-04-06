e7b870fd5ea0714aa85926d24c02a84f9aa25982
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-04-06 22:11:05 +0530

    Add files via upload

---
+++ HashTable/NumbersSmallerThanCurrent_9.java
@@ -0,0 +1,88 @@
+/*
+
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than
+it. That is, for each nums[i] you have to count the number of valid j's such that j != i
+and nums[j] < nums[i].
+Return the answer in an array.
+
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation:
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1).
+For nums[3]=2 there exist one smaller number than it (1).
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+Example 2:
+
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+
+Example 3:
+
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0]
+
+Constraints:
+
+2 <= nums.length <= 500
+0 <= nums[i] <= 100
+
+ */
+
+package HashTable;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class NumbersSmallerThanCurrent_9 {
+
+    public static void main(String[] args){
+        int[] arr = {8,1,2,2,3};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(arr)));
+    }
+
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] ans = Arrays.copyOf(nums,nums.length);
+        Arrays.sort(ans);
+        for(int i=0;i<nums.length;i++){
+            map.putIfAbsent(ans[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            ans[i]=map.get(nums[i]);
+        }
+        return ans;
+    }
+
+}
+
+/*
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    count++;
+                }
+            }
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]>nums[j]){
+                    count++;
+                }
+            }
+            ans[i]=count;
+            count=0;
+        }
+        return ans;
+    }
+}
+
+
+ */
