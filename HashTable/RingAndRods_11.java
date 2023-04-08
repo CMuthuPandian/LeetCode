200524986ee44a65457070baef092298c3d4c70e
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-04-08 10:24:30 +0530

    Update RingAndRods_11.java

--- HashTable/RingAndRods_11.java
+++ HashTable/RingAndRods_11.java
@@ -1,7 +1,6 @@
 /*
 
 There are n rings and each ring is either red, green, or blue. The rings are distributed across ten rods labeled from 0 to 9.
-
 You are given a string rings of length 2n that describes the n rings that are placed onto the rods. Every two characters in rings forms a color-position pair that is used to describe each ring where:
 
 The first character of the ith pair denotes the ith ring's color ('R', 'G', 'B').
