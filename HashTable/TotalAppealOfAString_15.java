ae25018fd7b09b298147b31761cb7381f7c6ede4
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-04-14 22:47:53 +0530

    Update TotalAppealOfAString_15.java

--- HashTable/TotalAppealOfAString_15.java
+++ HashTable/TotalAppealOfAString_15.java
@@ -1,7 +1,6 @@
 /*
 
 The appeal of a string is the number of distinct characters found in the string.
-
 For example, the appeal of "abbca" is 3 because it has 3 distinct characters: 'a', 'b', and 'c'.
 Given a string s, return the total appeal of all of its substrings.
 
