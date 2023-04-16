9012f3280e374c71d8b83567f33b571550616521
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-04-16 22:56:45 +0530

    Update UserActiveMinutes_17.java

--- HashTable/UserActiveMinutes_17.java
+++ HashTable/UserActiveMinutes_17.java
@@ -1,7 +1,6 @@
 /*
 
 You are given the logs for users' actions on LeetCode, and an integer k. The logs are represented by a 2D integer array logs where each logs[i] = [IDi, timei] indicates that the user with IDi performed an action at the minute timei.
-
 Multiple users can perform actions simultaneously, and a single user can perform multiple actions in the same minute.
 
 The user active minutes (UAM) for a given user is defined as the number of unique minutes in which the user performed an action on LeetCode. A minute can only be counted once, even if multiple actions occur during it.
