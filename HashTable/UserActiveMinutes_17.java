e282b1200a9a99dfd9093353280c49fb9bd68ef4
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-04-16 22:57:17 +0530

    Update UserActiveMinutes_17.java

--- HashTable/UserActiveMinutes_17.java
+++ HashTable/UserActiveMinutes_17.java
@@ -4,7 +4,6 @@ You are given the logs for users' actions on LeetCode, and an integer k. The log
 Multiple users can perform actions simultaneously, and a single user can perform multiple actions in the same minute.
 The user active minutes (UAM) for a given user is defined as the number of unique minutes in which the user performed an action on LeetCode. A minute can only be counted once, even if multiple actions occur during it.
 You are to calculate a 1-indexed array answer of size k such that, for each j (1 <= j <= k), answer[j] is the number of users whose UAM equals j.
-
 Return the array answer as described above.
 
 
